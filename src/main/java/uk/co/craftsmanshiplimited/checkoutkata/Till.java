561e23b9b0306a77aeb16057b46efa9b1f61300b
Henrik Eichenhardt <henrik.eichenhardt@gmail.com>
2017-03-12 12:59:42 +0000

    Refactoring: Move sum calculation from scan time to checout time.

--- src/main/java/uk/co/craftsmanshiplimited/checkoutkata/Till.java
+++ src/main/java/uk/co/craftsmanshiplimited/checkoutkata/Till.java
@@ -1,5 +1,8 @@
 package uk.co.craftsmanshiplimited.checkoutkata;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 import static uk.co.craftsmanshiplimited.checkoutkata.Till.ScannerResponse.ERROR;
 import static uk.co.craftsmanshiplimited.checkoutkata.Till.ScannerResponse.OK;
 
@@ -8,32 +11,48 @@ import static uk.co.craftsmanshiplimited.checkoutkata.Till.ScannerResponse.OK;
  */
 public class Till {
 
+    private List<String> items;
+    private Set<String> availableInInventory;
+
     enum ScannerResponse {
         OK, ERROR
+    }
+
+    public Till() {
+        this.items = new LinkedList<String>();
+        this.availableInInventory = new HashSet<String>(Arrays.asList("A", "B", "C", "D"));
     }
 
     private int sum;
 
     public ScannerResponse scan(final String stockKeepkingUnit) {
-        if(stockKeepkingUnit.equals("A")) {
-            sum += 50;
+        if(availableInInventory.contains(stockKeepkingUnit)) {
+            this.items.add(stockKeepkingUnit);
             return OK;
-        } else if (stockKeepkingUnit.equals("B")){
-            sum += 30;
-            return OK;
-        } else if (stockKeepkingUnit.equals("C")){
-            sum += 20;
-            return OK;
-        } else if (stockKeepkingUnit.equals("D")){
-            sum += 15;
-            return OK;
-        } else {
-            return  ERROR;
         }
+        return ERROR;
 
     }
 
     public int getSum() {
-        return sum;
+        final Map<String, Long> productToCount =
+                items.stream().collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+
+        return productToCount.entrySet().stream().mapToInt(entry -> calculatePrice(entry)).sum();
+    }
+
+    private int calculatePrice(final Map.Entry<String, Long> entry) {
+        final String stockKeepkingUnit = entry.getKey();
+        if(stockKeepkingUnit.equals("A")) {
+            return 50;
+        } else if (stockKeepkingUnit.equals("B")){
+            return 30;
+        } else if (stockKeepkingUnit.equals("C")){
+            return 20;
+        } else if (stockKeepkingUnit.equals("D")){
+            return 15;
+        } else {
+            throw new IllegalArgumentException("Unknown product in basket");
+        }
     }
 }
