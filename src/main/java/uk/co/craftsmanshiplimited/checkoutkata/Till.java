7b3ac1c2d901aa83d232327c762d875cfd2d2ea4
Henrik Eichenhardt <henrik.eichenhardt@gmail.com>
2017-03-12 10:45:15 +0000

    Test: Scan first item

---
+++ src/main/java/uk/co/craftsmanshiplimited/checkoutkata/Till.java
@@ -0,0 +1,17 @@
+package uk.co.craftsmanshiplimited.checkoutkata;
+
+/**
+ * Created by Henrik on 12/03/2017.
+ */
+public class Till {
+
+    private int sum;
+
+    public void scan(String a) {
+        sum += 50;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+}
---
+++ src/test/java/uk/co/craftsmanshiplimited/checkoutkata/TillTest.java
@@ -0,0 +1,26 @@
+package uk.co.craftsmanshiplimited.checkoutkata;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Henrik on 12/03/2017.
+ */
+public class TillTest {
+
+    private Till till;
+
+    @Before
+    public void setup() throws Exception {
+        this.till = new Till();
+    }
+
+    @Test
+    public void shouldScanFirstItem() throws Exception {
+        this.till.scan("A");
+        assertEquals(50, till.getSum());
+    }
+}
