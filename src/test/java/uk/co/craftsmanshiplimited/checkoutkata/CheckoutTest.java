eefd38bcb7d9c8e2ca7404e656aa3fb8e411734b
Henrik Eichenhardt <henrik.eichenhardt@gmail.com>
2017-03-12 15:22:59 +0000

    Test: Get the total price from the pricing strategy

--- src/test/java/uk/co/craftsmanshiplimited/checkoutkata/CheckoutTest.java
+++ src/test/java/uk/co/craftsmanshiplimited/checkoutkata/CheckoutTest.java
@@ -4,11 +4,17 @@ package uk.co.craftsmanshiplimited.checkoutkata;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static uk.co.craftsmanshiplimited.checkoutkata.Checkout.ScannerResponse.ERROR;
@@ -34,4 +40,14 @@ public class CheckoutTest {
         assertEquals(OK, checkout.scan("A"));
     }
 
+    @Test
+    public void shouldGetTheTotalPriceFromThePricingStrategy() throws Exception {
+        when(pricingStrategy.contains("A")).thenReturn(true);
+        when(pricingStrategy.contains("B")).thenReturn(true);
+        when(this.pricingStrategy.getTotalPrice(eq(Arrays.asList("A", "B")))).thenReturn(50);
+
+        this.checkout.scan("A");
+        this.checkout.scan("B");
+        assertEquals(50, this.checkout.getTotalPrice());
+    }
 }
